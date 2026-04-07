2851e36403b26f7ef13bd360ffc2a0f146681069
Declan <declanbowleycoding@gmail.com>
2026-04-07 13:00:27 -0400

    Added looping menu requiring user input, no options function except program exit at the moment.

--- Main.java
+++ Main.java
@@ -1,12 +1,74 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main (String[] args) {
 
+		Scanner console=new Scanner(System.in);
+
+		String choice;
+
+		boolean looper=true;
+
 		System.out.println("Welcome to the Codd Student Course Register System.");
-		//added line
-		
-		
+
+		while (looper==true) {
+		System.out.println("***************************************************");
+		System.out.println("1) Add Student");
+		System.out.println("2) Remove Student");
+		System.out.println("3) Add Course");
+		System.out.println("4) Remove Course");
+		System.out.println("5) View All Students"); 
+		System.out.println("6) View All Courses");
+		System.out.println("7) Exit Program");
+		System.out.println("***************************************************");
+
+			choice=console.nextLine();
+			
+			if(choice.equals("1")) {
+				System.out.println("Add student");
+				
+				
+			}else if(choice.equals("2")) {
+				System.out.println("Remove student");
+
+				
+			}else if(choice.equals("3")) {
+				System.out.println("Add course");
+				
+				
+			}else if(choice.equals("4")) {
+				System.out.println("Remove course");
+				
+				
+			}else if(choice.equals("5")) {
+				System.out.println("View all students");
+				
+				
+			}else if(choice.equals("6")) {
+				System.out.println("View all courses");
+				
+				
+			}else if(choice.equals("7")) {
+				System.out.println("Exit program");
+				break;
+				
+			}else {
+				System.out.println("Please try 1, 2, 3, 4, or 5");
+				
+			}
+
+		}
+
+
+		System.out.println("System ended");
 	}
+	//method land woohoo
+
+
+
+
+
+
+
 
 }
--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
-Poopoo peepee
+Console line Java program, started April 6th, 2026.
+
+Looping user menu: loops until valid input, executes option associated with input, sends you back to the menu unless menu exit was chosen, in which case the program ends.
