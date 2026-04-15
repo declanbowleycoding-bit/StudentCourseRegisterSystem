def0d291cb7f03b67cacabba4cd4c76c152d9339
Declan <declanbowleycoding@gmail.com>
2026-04-15 11:40:30 -0400

    Options 1 through 4 now have function, allowing you to create student objects, course objects, and you can view all stored student and course objects.

--- Main.java
+++ Main.java
@@ -1,54 +1,69 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
 
 	public static void main (String[] args) {
+		
+		ArrayList<Student> Students=new ArrayList<>();
+		ArrayList<Course> Courses=new ArrayList<>();
 
 		Scanner console=new Scanner(System.in);
 
 		String choice;
-
-		boolean looper=true;
 		
+		boolean looper=true;
+
 		System.out.println("Welcome to the Codd Student Course Register System.");
 
 		while (looper==true) {
 			System.out.println("***************************************************");
 			System.out.println("1) Add Student");
-			System.out.println("2) Remove Student");
-			System.out.println("3) Add Course");
-			System.out.println("4) Remove Course");
-			System.out.println("5) View All Students"); 
-			System.out.println("6) View All Courses");
-			System.out.println("7) Exit Program");
+			System.out.println("2) Add Course");
+			System.out.println("3) View All Students"); 
+			System.out.println("4) View All Courses");
+			System.out.println("5) Exit Program");
 			System.out.println("***************************************************");
 
 			choice=console.nextLine();
 
 			if(choice.equals("1")) {
-				System.out.println("Add student");
-
+				System.out.println("Type in the student's ID number.");
+				int intChoice=numberVerification();
+				System.out.println(intChoice);
+				System.out.println("Now type in the student's name.");
+				choice=console.nextLine();
+				System.out.println(choice);
+				System.out.println("Finally type in the student's grade level.");
+				int intChoice2=numberVerification();
+				System.out.println(intChoice2);
+				Students.add(new Student(intChoice, choice, intChoice2));
+				System.out.println(Students.get((Students.size())-1));
 
 			}else if(choice.equals("2")) {
-				System.out.println("Remove student");
-
+				System.out.println("Type in the courses's code.");
+				String choice2=console.nextLine();
+				System.out.println(choice2);
+				System.out.println("Now type in the course's name.");
+				choice=console.nextLine();
+				System.out.println(choice);
+				System.out.println("Finally type in the maximum student capacity");
+				int intChoice=numberVerification();
+				System.out.println(intChoice);
+				Courses.add(new Course(choice2, choice, intChoice));
+				System.out.println(Courses.get((Courses.size())-1));
 
 			}else if(choice.equals("3")) {
-				System.out.println("Add course");
-
+				for(Student e: Students) {
+					System.out.println(e);
+				}
 
 			}else if(choice.equals("4")) {
-				System.out.println("Remove course");
+				for(Course e: Courses) {
+					System.out.println(e);
+				}
 
 
 			}else if(choice.equals("5")) {
-				System.out.println("View all students");
-
-
-			}else if(choice.equals("6")) {
-				System.out.println("View all courses");
-
-
-			}else if(choice.equals("7")) {
 				System.out.println("Exit program");
 				break;
 
@@ -59,12 +74,26 @@ public class Main {
 
 		}
 
-
 		System.out.println("System ended");
 	}
 	//method land woohoo
 
+	public static int numberVerification() {
+		Scanner console=new Scanner(System.in);
 
+		int intChoice=0;
+	
+			if (console.hasNextInt()) {
+				intChoice=console.nextInt();
+
+			}else {
+				System.out.println("Please type a whole number.");
+				intChoice=numberVerification();
+				
+			}
+			
+		return intChoice;
+	}
 
 
 
--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 Console line Java program, started April 6th, 2026.
 
-Looping user menu: loops until valid input, executes option associated with input, sends you back to the menu unless menu exit was chosen, in which case the program ends.
+Looping user menu: loops until valid input, executes option associated with input, sends you back to the menu unless menu exit was chosen, in which case the program ends. Can add students and courses, and view all students and courses.
 
-Student object stores id, name, and grade level of student. It has getters, setters, and a toString.
+Student object: stores id, name, and grade level of student. It has getters, setters, and a toString.
 
-Course object storing course code, course name, and max students. Includes getters, setters, and a toString.
+Course object: stores course code, course name, and max students. Includes getters, setters, and a toString.
